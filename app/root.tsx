bcc986d532e39081301fa372fd2f6aa160e548af
gustavocadev <churaalaygustavo11@gmail.com>
2022-02-03 09:27:55 -0500

    I added light theme

--- app/root.tsx
+++ app/root.tsx
@@ -7,27 +7,47 @@ import {
     ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import styles from "./tailwind.css";
+import { ThemeContext } from "./context/context";
+import { useState, useEffect } from "react";
 
 export const meta: MetaFunction = () => {
     return { title: "New Remix App" };
 };
+export function links() {
+    return [{ rel: "stylesheet", href: styles }];
+}
 
 export default function App() {
+    const [isDarkMode, setIsDarkMode] = useState<boolean>();
+    useEffect(() => {
+        const actualTheme: boolean = JSON.parse(
+            localStorage.getItem("dark-theme") ?? "true"
+        );
+        setIsDarkMode(actualTheme);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("dark-theme", JSON.stringify(isDarkMode));
+    }, [isDarkMode]);
+
     return (
-        <html lang="en">
+        <html lang="en" className={`${isDarkMode ? "dark" : "light"}`}>
             <head>
                 <meta charSet="utf-8" />
                 <meta
                     name="viewport"
                     content="width=device-width,initial-scale=1"
                 />
-                <script src="https://cdn.tailwindcss.com"></script>
 
                 <Meta />
                 <Links />
             </head>
-            <body className="bg-[#F7F6F3] text-[#2d3436] dark:bg-[#121212] dark:text-gray-200">
-                <Outlet />
+            <body className="dark:bg-[#121212] dark:text-gray-200 bg-[#F7F6F3] text-[#2d3436]">
+                <ThemeContext.Provider value={[isDarkMode, setIsDarkMode]}>
+                    <Outlet />
+                </ThemeContext.Provider>
+
                 <ScrollRestoration />
                 <Scripts />
                 {process.env.NODE_ENV === "development" && <LiveReload />}
